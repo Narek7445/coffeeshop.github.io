79fc9e0db6bd9cccf4bdaea6a47108ef9a95afec
Narek7445 <132198346+Narek7445@users.noreply.github.com>
2026-03-22 17:16:51 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const productsGrid = document.querySelector('.products-grid');
 
-let product = document.createElement('div')
-product.innerHTML = `
-<div class="product-container">
-    <img src="assets/products/rusberry-latte.jpg" alt="Картинка продукта" class="product-image">
-    <p class="product-description">Малиновый латте</p>
-    <p class="product-cost">470 ₽</p>
-    <button class="product-addtocart">Добавить</button>
-</div>
-`
+let menu = {
+    drinks : [
+        {
+            name: "Малиновый латте",
+            cost: 470
+        }
+    ]
+}
+
+function createProductFrame(category){
+    let product = document.createElement('div')
+
+    product.innerHTML = `
+        <div class="product-container">
+            <img src="assets/products/rusberry-latte.jpg" alt="Картинка продукта" class="product-image">
+            <p class="product-description">Малиновый латте</p>
+            <p class="product-cost">470 ₽</p>
+            <button class="product-addtocart">Добавить</button>
+        </div>
+    `
+
+    document.querySelector('.products-grid').append(product);
+}
 
 function config(){
     configCategories();
@@ -17,6 +31,7 @@ function config(){
 function configCategories(){
     let categories = document.querySelector('#product-categories');
     let openCategories = document.querySelector('#categories-button');
+    let closeCategories = document.querySelector('#close-categories');
 
     let defaultLeft = categories.style.left;
     let debounce = false;
@@ -27,12 +42,20 @@ function configCategories(){
         if (debounce) {return};
         debounce = true;
         
-        categories.style.left = defaultLeft;
+        categories.style.left = (categories.style.left == defaultLeft) ? '-50%' : defaultLeft;
 
         setTimeout(() => {
             debounce = false;
         }, 1000);
     })
+
+    closeCategories.addEventListener('click',()=>{
+        categories.style.left = '-50%';
+    })
 }
 
 config();
+
+for (let product in menu){
+    console.log(menu[product]);
+}
